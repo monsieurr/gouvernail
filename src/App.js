60455e24deb4cefee2e914ce17922bcc530b85cb
monsieurr <plouftoto@gmail.com>
2021-08-05 05:01:24 +0200

    first commit, adding the base

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import './App.css';
+import TravelForm from './components/TravelForm';
+import TravelJournal from './components/TravelJournal';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+		  <h1>Gouvernail</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
